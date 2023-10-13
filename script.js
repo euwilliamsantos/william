daed28af03dc3dceb427bab866db9bf5def88e49
euwilliamsantos <145413257+euwilliamsantos@users.noreply.github.com>
2023-10-12 22:12:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-let score = 0;
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
+const start = document.querySelector('.start')
+const gameOver = document.querySelector('.game-over')
+
+audioStart = new Audio('./audio-theme.mp3')
+audioGameOver = new Audio('./audio_gameover.mp3')
+
+
+const startGame = () => {
+  pipe.classList.add('pipe-animation')
+  start.style.display = 'none'
+
+  // audio
+    audioStart.play();
+    audioStart.loop = true;
+}
+
+const restartGame = () => {
+  gameOver.style.display = 'none'
+  pipe.style.left = ''
+  pipe.style.right = '0'
+  mario.src = './img/mario.gif'
+  mario.style.width = '150px'
+  mario.style.bottom = '0'
+
+  start.style.display = 'none'
+
+  audioGameOver.pause()
+  audioGameOver.currentTime = 0;
+
+  audioStart.play()
+  audioStart.currentTime = 0;
+
+}
+
+const jump = () => {
+  mario.classList.add('jump')
+
+  setTimeout(() => {
+    mario.classList.remove('jump')
+  }, 800)
+}
+
 const loop = () => {
   setInterval(() => {
     const pipePosition = pipe.offsetLeft
@@ -7,33 +51,54 @@ const loop = () => {
       .bottom.replace('px', ' ')
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-      // ... (o restante do código permanece o mesmo)
+      pipe.classList.remove('.pipe-animation')
+      pipe.style.left = `${pipePosition}px`
+
+      mario.classList.remove('.jump')
+      mario.style.bottom = `${marioPosition}px`
+
+      mario.src = './img/game-over.png'
+      mario.style.width = '80px'
+      mario.style.marginLeft = '50px'
+
+
+      function stopAudioStart() {
+        audioStart.pause()
+      }
+      stopAudioStart()
+
+      audioGameOver.play()
+
+      function stopAudio() {
+        audioGameOver.pause()
+      }
+      setTimeout(stopAudio, 7000)
 
       gameOver.style.display = 'flex'
 
       clearInterval(loop)
-    } else if (pipePosition <= 0) {
-      score++; // Incrementa a pontuação quando passa pelo obstáculo
-    }
-  }, 10)
-}
-const loop = () => {
-  setInterval(() => {
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = window
-      .getComputedStyle(mario)
-      .bottom.replace('px', ' ')
-
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-      // ... (o restante do código permanece o mesmo)
-
-      gameOver.style.display = 'flex'
-
-      clearInterval(loop)
-    } else if (pipePosition <= 0) {
-      score++; // Incrementa a pontuação quando passa pelo obstáculo
-      document.getElementById('score').textContent = score; // Atualiza o elemento de pontuação
     }
   }, 10)
 }
 
+loop()
+
+document.addEventListener('keypress', e => {
+  const tecla = e.key
+  if (tecla === ' ') {
+    jump()
+  }
+})
+
+document.addEventListener('touchstart', e => {
+  if (e.touches.length) {
+    jump() 
+  }
+})
+
+document.addEventListener('keypress', e => {
+  const tecla = e.key
+  if (tecla === 'Enter') {
+    startGame()
+  }
+})
